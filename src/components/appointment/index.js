216b20e73f070cc09a21fb1205c33b62cc8b2c17
anthonyshadow <anthony.shadowitz@gmail.com>
2019-10-29 15:14:28 -0400

    add displaying appointment functions for when booked and empty

--- src/components/appointment/index.js
+++ src/components/appointment/index.js
@@ -3,6 +3,14 @@ import React from "react";
 
 import "components/appointment/styles.scss";
 
+import Header from "components/appointment/Header"
+import Empty from "components/appointment/Empty"
+import Show from "components/appointment/Show"
+
 export default function Appointment(props) {
-  return (<article className="appointment"></article>);
+  return <article className="appointment">
+    < Header time={props.time} />
+    {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer} />}
+    {!props.interview && <Empty />}
+  </article>;
 }
