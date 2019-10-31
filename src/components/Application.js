4d3291dccdcd47e31215083db2a7b739f14e7310
anthonyshadow <anthony.shadowitz@gmail.com>
2019-10-31 12:55:47 -0400

    add axios, retrieve days, appointments, interviewer data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,56 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 import "components/Application.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem"
 import DayList from "components/DayList"
+import Appointment from "components/appointment"
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+
+
+
 
 
 export default function Application(props) {
-  const [ day, setDay ] = useState("Monday")
+  const [days, setDays] = useState([])
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  
+
+useEffect(() => {
+  Promise.all([
+    Promise.resolve(axios.get("/api/days")),
+    Promise.resolve(axios.get(" /api/appointments")),
+    Promise.resolve(axios.get("/api/interviewers"))
+  ]).then((all) => {
+    console.log(all); // first
+    setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+  });
+}, [])
+
+  
   return (
     <main className="layout">
       <section className="sidebar">
-      <img
-  className="sidebar--centered"
-  src="images/logo.png"
-  alt="Interview Scheduler"
-/>
-<hr className="sidebar__separator sidebar--centered" />
-<nav className="sidebar__menu">
-<DayList
-  days={days}
-  day={day}
-  setDay={setDay}
-/>
-</nav>
-<img
-  className="sidebar__lhl sidebar--centered"
-  src="images/lhl.png"
-  alt="Lighthouse Labs"
-/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <DayList
+            days={state.days}
+            day={state.day}
+            setDay={setDay}
+          />
+        </nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {getAppointmentsForDay(state, state.day).map(appointment => {
+          const interview = getInterview(state, appointment.interview)
+          return <Appointment
+            key={appointment.id}
+            id={appointment.id}
+            time={appointment.time}
+            interview={interview} />
+        })}
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
 }
+
+
+
